4105a09d7dc99a4b86012888cfb795dca7eef143
Laj01 <nagylali001@gmail.com>
2020-04-05 13:01:33 +0200

    Main class modified

--- src/main/java/legoset/Main.java
+++ src/main/java/legoset/Main.java
@@ -1,48 +1,48 @@
 package legoset;
 
+import jaxb.JAXBHelper;
+
+import javax.xml.bind.JAXBException;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.time.Year;
 import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.xml.bind.JAXBContext;
-import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
-
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException, JAXBException {
 
-        LegoSet set = new LegoSet();
-        set.setName("Imperial TIE Fighter");
-        set.setNumber("75211");
-        set.setTheme("Star Wars");
-        set.setSubtheme("Solo");
-        set.setYear(Year.parse("2018"));
-        set.setPieces(519);
+        LegoSet legoset = new LegoSet();
+        legoset.setName("Imperial TIE Fighter");
+        legoset.setNumber("75211");
+        legoset.setTheme("Star Wars");
+        legoset.setSubtheme("Solo");
+        legoset.setYear(Year.parse("2018"));
+        legoset.setPieces(519);
+
         Set<String> tags = new LinkedHashSet<String>();
         tags.add("Starfighter");
         tags.add("Stormtrooper");
         tags.add("Star Wars");
         tags.add("Solo");
-        set.setTags(tags);
+        legoset.setTags(tags);
+
         List<Minifig> minifigs = new ArrayList<Minifig>();
         minifigs.add(new Minifig("Imperial Mudtrooper", 2));
         minifigs.add(new Minifig("Imperial Pilot", 1));
         minifigs.add(new Minifig("Mimban Stormtrooper", 1));
-        set.setMinifigs(minifigs);
-        set.setWeight(new Weight("kg", 0.89));
-        set.setUrl("https://brickset.com/sets/75211-1/Imperial-TIE-Fighter");
 
-        try {
-            JAXBContext context = JAXBContext.newInstance(LegoSet.class);
-            Marshaller marshaller = context.createMarshaller();
-            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-            marshaller.setProperty(Marshaller.JAXB_ENCODING, "UTF-8");
-            marshaller.marshal(set, System.out);
-        } catch(JAXBException e) {
-            System.out.println(e.getMessage());
-        }
+        legoset.setMinifigs(minifigs);
+        legoset.setWeight(new Weight("kg", 0.89));
+        legoset.setUrl("https://brickset.com/sets/75211-1/Imperial-TIE-Fighter");
+
+        JAXBHelper.toXML(legoset, System.out);
+
+        JAXBHelper.toXML(legoset, new FileOutputStream("legoset.xml"));
+        System.out.println(JAXBHelper.fromXML(LegoSet.class, new FileInputStream("legoset.xml")));
     }
 }
