aa22b69ba5db3128b1752ddc25a4798171b2dbf9
Laj01 <nagylali001@gmail.com>
2020-04-05 12:29:42 +0200

    LegoSet class added

---
+++ src/main/java/legoset/LegoSet.java
@@ -0,0 +1,37 @@
+package legoset;
+
+import lombok.Data;
+import movie.YearAdapter;
+
+import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
+import java.time.Year;
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(propOrder = {"name", "theme", "subtheme", "year", "pieces", "tags", "minifigs", "weight", "url" })
+@Data
+public class LegoSet {
+
+    @XmlAttribute
+    private String number;
+    private String name;
+    private String subtheme;
+
+    @XmlJavaTypeAdapter(YearAdapter.class)
+    private Year year;
+    private int pieces;
+
+    @XmlElementWrapper(name = "tags")
+    @XmlElement(name = "tag")
+    private java.util.Set<String> tags;
+
+    @XmlElementWrapper(name = "minifigs")
+    @XmlElement(name = "minifig")
+    private java.util.List<Minifig> minifigs;
+
+
+    private Weight weight;
+    private String url;
+
+}
